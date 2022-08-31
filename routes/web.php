a054e25d7df09142f04f15bd77be05064593e6bd
jose junior <jose.junior@prodepa.pa.org.br>
2022-08-30 21:03:54 -0300

    video 7 seed, banco de dados alimentado com um usuário

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/users', [UserController::class, 'index'])->name('user.index');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('user.show');
 Route::get('/', function () {
     return view('welcome');
 });
